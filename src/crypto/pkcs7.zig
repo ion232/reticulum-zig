a2f8964e6c7791162eb576b695b15bde06f1c837
Arran Ireland <a.ireland.git@proton.me>
2024-12-11 19:00:06 +0000

    crypto: use slices in pkcs7

--- src/crypto/pkcs7.zig
+++ src/crypto/pkcs7.zig
@@ -5,12 +5,14 @@ pub const UnpadError = error{
     InvalidPadValue,
 };
 
-pub fn pad(data: []u8, out: []u8, comptime block_length: u8) []u8 {
-    const r: u8 = @intCast(data.items.len % block_length);
+// Wrap this in a comptime struct.
+
+pub fn pad(comptime block_length: u8, data: []const u8, out: []u8) []u8 {
+    const r: u8 = @intCast(data.len % block_length);
     const n = block_length - r;
     std.debug.assert(out.len >= data.len);
 
-    @memcpy(out, data);
+    @memcpy(out[0..data.len], data[0..data.len]);
 
     for (0..n) |i| {
         out[data.len + i] = n;
@@ -19,7 +21,7 @@ pub fn pad(data: []u8, out: []u8, comptime block_length: u8) []u8 {
     return out[0 .. data.len + n];
 }
 
-pub fn unpad(data: []u8, out: []u8, comptime block_length: u8) ![]u8 {
+pub fn unpad(comptime block_length: u8, data: []const u8) ![]const u8 {
     if (data.len == 0 or (data.len % block_length) != 0) {
         return UnpadError.InvalidLength;
     }
@@ -35,97 +37,94 @@ pub fn unpad(data: []u8, out: []u8, comptime block_length: u8) ![]u8 {
         }
     }
 
-    std.debug.assert(out.len >= data.len - n);
-
-    // Does this need to be copied?
-    @memcpy(out, data[0 .. data.len - n]);
-
-    return out[0 .. data.len - n];
+    return data[0 .. data.len - n];
 }
 
+const t = std.testing;
+
 test "pad - empty" {
-    const allocator = std.testing.allocator;
     const block_length = 16;
 
-    var data = std.ArrayList(u8).init(allocator);
-    defer data.deinit();
-    try pad(&data, block_length);
-    try std.testing.expectEqual(16, data.items.len);
-    try std.testing.expectEqual(16, data.items[0]);
+    const data: [0]u8 = undefined;
+    var out: [block_length]u8 = undefined;
+
+    const padded = pad(block_length, &data, &out);
+    try t.expectEqual(block_length, padded.len);
+    try t.expectEqual(block_length, padded[0]);
 }
 
 test "pad - length 1" {
-    const allocator = std.testing.allocator;
     const block_length = 16;
 
-    var data = std.ArrayList(u8).init(allocator);
-    defer data.deinit();
-    try data.append(1);
-    try pad(&data, block_length);
-    try std.testing.expectEqual(@as(usize, 16), data.items.len);
-    try std.testing.expectEqual(@as(u8, 15), data.items[data.items.len - 1]);
+    var data: [1]u8 = undefined;
+    var out: [block_length]u8 = undefined;
+
+    data[0] = 1;
+
+    const padded = pad(block_length, &data, &out);
+    try t.expectEqual(@as(usize, block_length), padded.len);
+    try t.expectEqual(@as(u8, block_length - 1), padded[padded.len - 1]);
 }
 
 test "pad - block size" {
-    const allocator = std.testing.allocator;
     const block_length = 16;
 
-    var data = std.ArrayList(u8).init(allocator);
-    defer data.deinit();
-    try data.appendNTimes(1, block_length);
-    try pad(&data, block_length);
-    try std.testing.expectEqual(@as(usize, 32), data.items.len);
-    try std.testing.expectEqual(@as(u8, 16), data.items[data.items.len - 1]);
+    var data: [block_length]u8 = undefined;
+    var out: [2 * block_length]u8 = undefined;
+
+    @memset(data[0..block_length], 1);
+
+    const padded = pad(block_length, &data, &out);
+    try t.expectEqual(@as(usize, 2 * block_length), padded.len);
+    try t.expectEqual(@as(u8, block_length), padded[padded.len - 1]);
 }
 
 test "unpad - valid one block" {
-    const allocator = std.testing.allocator;
     const block_length = 16;
 
-    var data = std.ArrayList(u8).init(allocator);
-    defer data.deinit();
-    try data.appendNTimes(4, 16);
-    try unpad(&data, block_length);
-    try std.testing.expectEqual(@as(usize, 12), data.items.len);
+    var data: [block_length]u8 = undefined;
+    @memset(data[0..block_length], 4);
+
+    const unpadded = try unpad(block_length, &data);
+    try t.expectEqual(@as(usize, block_length - 4), unpadded.len);
 }
 
 test "unpad - valid two blocks" {
-    const allocator = std.testing.allocator;
     const block_length = 16;
+    const real_data_length = 2 * (block_length - 1);
 
-    var data = std.ArrayList(u8).init(allocator);
-    defer data.deinit();
-    try data.appendNTimes(1, 30);
-    try data.appendNTimes(2, 2);
-    try unpad(&data, block_length);
-    try std.testing.expectEqual(@as(usize, 30), data.items.len);
+    var data: [2 * block_length]u8 = undefined;
+
+    @memset(data[0..real_data_length], 1);
+    @memset(data[real_data_length .. real_data_length + 2], 2);
+
+    const unpadded = try unpad(block_length, &data);
+    try t.expectEqual(@as(usize, real_data_length), unpadded.len);
 }
 
 // ion232: A value that's too small can't be tested for. I don't think it actually matters though in practice.
 
 test "unpad - value too big" {
-    const allocator = std.testing.allocator;
     const block_length = 16;
 
-    var data = std.ArrayList(u8).init(allocator);
-    defer data.deinit();
+    var data: [block_length]u8 = undefined;
 
-    try data.appendNTimes(1, 15);
-    try data.append(17);
+    @memset(data[0 .. block_length - 1], 1);
+    data[block_length - 1] = block_length + 1;
 
-    try std.testing.expectError(UnpadError.InvalidPadValue, unpad(&data, block_length));
+    const unpadded = unpad(block_length, &data);
+    try t.expectError(UnpadError.InvalidPadValue, unpadded);
 }
 
-test "unpad - invalid sum" {
-    const allocator = std.testing.allocator;
+test "unpad - invalid values" {
     const block_length = 16;
 
-    var data = std.ArrayList(u8).init(allocator);
-    defer data.deinit();
+    var data: [block_length]u8 = undefined;
 
-    try data.appendNTimes(1, 14);
-    try data.append(0);
-    try data.append(2);
+    @memset(data[0 .. block_length - 2], 1);
+    data[block_length - 2] = 0;
+    data[block_length - 1] = 2;
 
-    try std.testing.expectError(UnpadError.InvalidSum, unpad(&data, block_length));
+    const unpadded = unpad(block_length, &data);
+    try t.expectError(UnpadError.InvalidPadValue, unpadded);
 }
