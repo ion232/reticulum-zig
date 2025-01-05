7105e1def5c9923e191432e758db8e2337ce6f95
Arran Ireland <a.ireland.git@proton.me>
2025-01-05 22:16:59 +0000

    core: add internal files

---
+++ core/internal/ThreadSafeFifo.zig
@@ -0,0 +1,45 @@
+const std = @import("std");
+const Allocator = std.mem.Allocator;
+const LinearFifo = std.fifo.LinearFifo;
+
+pub fn ThreadSafeFifo(comptime T: type) type {
+    return struct {
+        pub const Error = Allocator.Error;
+
+        const Self = @This();
+        const Impl = LinearFifo(T, .Dynamic);
+
+        mutex: std.Thread.Mutex,
+        impl: Impl,
+
+        pub fn init(ally: Allocator) Error!Self {
+            return Self{
+                .mutex = .{},
+                .impl = Impl.init(ally),
+            };
+        }
+
+        pub fn deinit(self: *Self, ally: Allocator) void {
+            self.mutex.lock();
+            self.impl.deinit(ally);
+            self.mutex.unlock();
+            self.* = undefined;
+        }
+
+        pub fn push(self: *Self, element: T) Error!void {
+            self.mutex.lock();
+            defer {
+                self.mutex.unlock();
+            }
+            try self.impl.writeItem(element);
+        }
+
+        pub fn pop(self: *Self) ?T {
+            self.mutex.lock();
+            defer {
+                self.mutex.unlock();
+            }
+            return self.impl.readItem();
+        }
+    };
+}
