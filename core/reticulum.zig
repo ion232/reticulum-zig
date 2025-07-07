6114e1873464681c813480aba921b2aff17f7352
Arran Ireland <a.ireland.git@proton.me>
2025-07-07 17:32:06 +0100

    test: fix unit tests not running with zig build

--- core/reticulum.zig
+++ core/reticulum.zig
@@ -10,3 +10,17 @@ pub const Packet = packet.Packet;
 pub const Interface = @import("Interface.zig");
 pub const Node = @import("Node.zig");
 pub const System = @import("System.zig");
+
+comptime {
+    const builtin = @import("builtin");
+    const std = @import("std");
+
+    // Ensure unit tests are ran by referencing relevant files.
+    if (builtin.is_test) {
+        // Private files must be specifically enumerated.
+        std.testing.refAllDecls(@import("crypto/Aes.zig"));
+        // Fernet currently needs fixing.
+        // Public files can be referenced all at once from here.
+        std.testing.refAllDecls(@This());
+    }
+}
