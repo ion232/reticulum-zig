9e620ca32b91c05557eb16d3bf13f9190d40bfa4
Arran Ireland <a.ireland.git@proton.me>
2025-07-08 11:35:00 +0100

    core: move system contents and remove dir

--- core/system/Clock.zig
+++
@@ -1,8 +0,0 @@
-const Self = @This();
-
-ptr: *anyopaque,
-monotonicMicrosFn: *const fn (ptr: *anyopaque) u64,
-
-pub fn monotonicMicros(self: *Self) u64 {
-    return self.monotonicMicrosFn(self.ptr);
-}
