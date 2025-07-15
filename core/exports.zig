ab8170f186acbd3df16d8fbfc28545085c08555b
Arran Ireland <a.ireland.git@proton.me>
2025-07-15 11:28:06 +0100

    core: decouple bindings generation from symbol exports

--- core/exports.zig
+++ core/exports.zig
@@ -1,12 +1,11 @@
-//! This file is not part of the core module and therefore can contain non-freestanding code.
-//! Be aware that this file will be imported into the build executable in order to generate the header file.
+//! These exports are currently unstable as the codebase develops.
 
+const bindings = @import("bindings.zig");
 const builtin = @import("builtin");
 const std = @import("std");
 const lib = @import("lib.zig");
 
-const is_wasm = (builtin.cpu.arch == .wasm32 or builtin.cpu.arch == .wasm64);
-
+const is_wasm = builtin.target.cpu.arch.isWasm();
 const Gpa = if (is_wasm) void else std.heap.GeneralPurposeAllocator(.{});
 const Allocator = std.mem.Allocator;
 
@@ -18,8 +17,7 @@ var clock: lib.System.SimpleClock = undefined;
 var rng: lib.System.SimpleRng = undefined;
 var system: lib.System = undefined;
 
-// Exported structs and functions.
-
+/// Error type.
 pub const Error = enum(c_int) {
     none = 0,
     already_initialized = 1,
@@ -29,7 +27,8 @@ pub const Error = enum(c_int) {
     unknown = 255,
 };
 
-pub fn libInit(
+/// Sets up the library.
+pub fn init(
     monotonicMicros: lib.System.SimpleClock.Callback,
     rngFill: lib.System.SimpleRng.Callback,
 ) callconv(.c) Error {
@@ -52,12 +51,11 @@ pub fn libInit(
         .rng = rng.rng(),
     };
 
-    // std.debug.print("Hello from a wasm module!\n", .{});
-
     return .none;
 }
 
-pub fn libDeinit() callconv(.c) Error {
+/// Tears down the library.
+pub fn deinit() callconv(.c) Error {
     allocator = null;
 
     if (is_wasm) {
@@ -71,10 +69,11 @@ pub fn libDeinit() callconv(.c) Error {
     return .none;
 }
 
+/// Makes a node.
 pub fn makeNode(node_ptr: **anyopaque) callconv(.c) Error {
     const ally = allocator orelse return .missing_allocator;
 
-    const node = ally.create(lib.Node) catch |e| return convertError(Allocator.Error, e);
+    const node = ally.create(lib.Node) catch |err| return convertError(Allocator.Error, err);
     errdefer ally.destroy(node);
 
     node_ptr.* = node;
@@ -83,14 +82,14 @@ pub fn makeNode(node_ptr: **anyopaque) callconv(.c) Error {
         &system,
         null,
         .{},
-    ) catch |e| return convertError(lib.Node.Error, e);
+    ) catch |err| return convertError(lib.Node.Error, err);
 
     return .none;
 }
 
-fn convertError(comptime E: type, e: E) Error {
+fn convertError(comptime E: type, err: E) Error {
     if (E == lib.Node.Error) {
-        return switch (e) {
+        return switch (err) {
             error.OutOfMemory => .out_of_memory,
             else => .unknown,
         };
@@ -99,104 +98,19 @@ fn convertError(comptime E: type, e: E) Error {
     return .unknown;
 }
 
-// Perform exports.
-// This is currently also adding these symbols to the build executable.
-// As far as I can tell it won't cause any issues and can probably be changed later.
 comptime {
     for (@typeInfo(@This()).@"struct".decls) |declaration| {
         const field = @field(@This(), declaration.name);
         const info = @typeInfo(@TypeOf(field));
 
         if (info != .@"fn") continue;
-        if (std.mem.eql(u8, declaration.name, "generateHeader")) continue;
 
         const function: *const anyopaque = @ptrCast(&field);
         const export_options = std.builtin.ExportOptions{
-            .name = deriveName(declaration.name),
+            .name = bindings.derive(declaration.name),
             .linkage = .strong,
         };
 
         @export(function, export_options);
-    }
-}
-
-/// This needs to be public for use in the build c step.
-pub fn generateHeader() []const u8 {
-    comptime var header: []const u8 =
-        \\#ifndef RT_CORE_H
-        \\#define RT_CORE_H
-        \\
-        \\#include <stddef.h>
-        \\#include <stdint.h>
-        \\
-        \\
-    ;
-
-    inline for (@typeInfo(Self).@"struct".decls) |declaration| {
-        const field = @field(Self, declaration.name);
-        const info = @typeInfo(@TypeOf(field));
-
-        if (info != .@"fn") continue;
-        if (comptime std.mem.eql(u8, declaration.name, "generateHeader")) continue;
-
-        const function = info.@"fn";
-        const name = comptime deriveName(declaration.name);
-        const return_type = switch (function.return_type.?) {
-            Error => "int",
-            *anyopaque => "void*",
-            c_int => "int",
-            else => @typeName(function.return_type.?),
-        };
-
-        comptime var forward_declaration: []const u8 = return_type ++ " " ++ name ++ "(";
-
-        inline for (function.params, 0..) |param, i| {
-            const param_type = switch (param.type.?) {
-                lib.System.SimpleClock.Callback => "int64_t (*monotonic_micros)(void)",
-                lib.System.SimpleRng.Callback => "void (*rng_fill)(uint8_t* buf, size_t length)",
-                *anyopaque => "void*",
-                **anyopaque => "void**",
-                c_int => "int",
-                else => @typeName(param.type.?),
-            };
-
-            forward_declaration = forward_declaration ++ param_type;
-
-            if (i != function.params.len - 1) {
-                forward_declaration = forward_declaration ++ ", ";
-            }
-        }
-
-        forward_declaration = forward_declaration ++ ");\n";
-
-        header = header ++ forward_declaration;
-    }
-
-    header = header ++
-        \\
-        \\#endif
-        \\
-    ;
-
-    return header;
-}
-
-fn deriveName(comptime name: []const u8) []const u8 {
-    var c_name: []const u8 = "rt_";
-
-    inline for (name) |char| {
-        if (std.ascii.isUpper(char)) {
-            c_name = c_name ++ .{ '_', std.ascii.toLower(char) };
-        } else {
-            c_name = c_name ++ .{char};
-        }
-    }
-
-    const arch = builtin.target.cpu.arch;
-
-    if (arch == .wasm32 or arch == .wasm64) {
-        return name;
-    } else {
-        return c_name;
     }
 }
